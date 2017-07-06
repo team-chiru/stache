0d1937c23dc7a6f3cd4d4499b7abe045faa6945a
vlnk <vlnk@protonmail.ch>
2017-07-06 01:57:01 -0400

    rewrite main exemple

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+extern crate stachemu;
+
+use stachemu::compile;
+use stachemu::file;
+use stachemu::engines::mustache::Builder;
+use stachemu::engines::processor::{ TemplateEngine, Engine };
+
+extern crate serde_json;
+
+fn main() {
+    let mustache = file::read("samples/interpolation.mustache").unwrap();
+    let template = compile(mustache).unwrap();
+
+    let json = file::read("samples/interpolation.json").unwrap();
+    let data = serde_json::from_str(&json).unwrap();
+    println!("{:?}", data);
+
+    let mut builder = Builder::configure(data);
+    println!("{}", builder.process(template).unwrap());
+}
